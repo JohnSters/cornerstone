cb6e3baf13f6e4d61917197a7db6e8f7900f0a4f
jonathan <jonnyschoeman@gmail.com>
2022-02-24 11:53:00 +0200

    First Commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app>
+    <v-main>
+      <MainAppBar/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import MainAppBar from './components/MainAppBar.vue'
+
+export default {
+  name: 'App',
+
+  components: {
+    MainAppBar,
+  },
+
+  data: () => ({
+    //
+  }),
+}
+</script>
