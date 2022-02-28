22702d429b0d26e29226d2fe8a1cbfc040ecee8b
jonathan <jonnyschoeman@gmail.com>
2022-02-28 12:34:27 +0200

    Added some filler content to front page and admin page.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,25 @@
 <template>
   <MainAppBar />
-  <v-container>This is the Home page</v-container>
+  <v-container>
+    <v-row>
+      <v-col
+          class="mt-3"
+          v-for="n in 9"
+          :key="n"
+          cols="4"
+      >
+        <v-card-title primary-title class="justify-center">
+          <div>
+            <h3 class="headline pink--text text--accent-2">Lorum</h3>
+            <div>Lorem ipsum dolor sit amet.</div>
+          </div>
+        </v-card-title>
+        <v-card-actions class="justify-center">
+          <v-btn flat color="green">use backend</v-btn>
+        </v-card-actions>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
